9f5bbc481cf144dedd01f3a2d967be3eb475681b
Tgx Bots <82932839+TgxBots@users.noreply.github.com>
2021-05-11 17:54:34 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Bin-Checker
-A Telegam Bin-Checker Bot to check is your bin is valid or not.
+A Telegram Bin-Checker Bot to check is your bins are valid or not.
+
+## Additional Information
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+
+<a href="https://t.me/TgxSupportChat"> <img src="https://img.shields.io/badge/telegram-Support_Group-red?style=social&logo=telegram" alt="Support" /></a>
+<a href="https://github.com/TgxBotz/Bin-Checker/stargazers"><img src="https://img.shields.io/github/stars/TgxBotz/Bin-Checker?style=social"></a><a href="https://github.com/TgxBotz/Bin-Checker/fork"><img src="https://img.shields.io/github/forks/TgxBotz/Bin-Checker?label=Fork&logoColor=blue&style=social"></a>	
+
+## Vars
+``` 
+API_ID - Your Telegram API ID
+API_HASH - Your Telegram API HASH
+TOKEN - Your Bot Token
+```
+
+# Deploy To Heroku
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/TgxBotz/Bin-Checker)
---
+++ bot.py
@@ -0,0 +1,86 @@
+import requests
+from telethon import events, Button, TelegramClient
+import os
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
+try:
+    API_ID = int(os.environ.get("API_ID", 6))
+    API_HASH = os.environ.get("API_HASH", None)
+    TOKEN = os.environ.get("TOKEN", None)
+except ValueError:
+    print("You forgot to fullfill vars")
+    print("Bot is quitting....")
+    exit()
+except Exception as e:
+    print(f"Error - {str(e)}")
+    print("Bot is quitting.....")
+    exit()
+except ApiIdInvalidError:
+    print("Your API_ID or API_HASH is Invalid.")
+    print("Bot is quitting.")
+    exit()
+
+bot = TelegramClient('bin', API_ID, API_HASH)
+bin = bot.start(bot_token=TOKEN)
+
+@bin.on(events.NewMessage(pattern="^[!?/]start$"))
+async def start(event):
+    if event.is_group:
+        await event.reply("**Bin-Checker is Alive**")
+        return
+    await event.reply(f"**Heya {event.sender.first_name}\nIts a Bin-Checker Bot To Check Your Bins Are Valid Or Not.", buttons=[
+    [Button.url("Mʏ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", "https://github.com/TgxBotz/Bin-Checker")]
+    ])
+
+@bin.on(events.NewMessage(pattern="^[!?/]help$"))
+async def help(event):
+    text = """
+**Welcome to HelpMenu:**
+
+- /start - To Start Me :)
+- /help - To Get Help Menu
+- /bin - To check is your bin valid or not
+"""
+    await event.reply(text, buttons=[[Button.url("Mʏ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", "https://github.com/TgxBotz/Bin-Checker")]])
+
+@bin.on(events.NewMessage(pattern="^[!?/]bin"))
+async def binc(event):
+    xx = await event.reply("`Processing.....`")
+    try:
+        input = event.text.split(" ", maxsplit=1)[1]
+
+        url = requests.get(f"https://bins-su-api.now.sh/api/{input}")
+        res = url.json()
+        vendor = res['data']['vendor']
+        type = res['data']['type']
+        level = res['data']['level']
+        bank = res['data']['bank']
+        country = res['data']['country']
+        me = (await event.client.get_me()).username
+
+        valid = f"""
+<b>➤ Valid Bin:</b>
+
+<b>Bin -</b> <code>{input}</code>
+<b>Status -</b> <code>Valid Bin</code>
+<b>Vendor -</b> <code>{vendor}</code>
+<b>Type -</b> <code>{type}</code>
+<b>Level -</b> <code>{level}</code>
+<b>Bank -</b> <code>{bank}</code>
+<b>Country -</b> <code>{country}</code>
+
+<b>Checked By - @{me}</b>
+<b>User-ID - {event.sender_id}</b>
+"""
+        await xx.edit(valid, parse_mode="HTML")
+    except IndexError:
+       await xx.edit("Plese provide a bin to check\n__`/bin yourbin`__")
+    except KeyError:
+        me = (await event.client.get_me()).username
+        await xx.edit(f"**➤ Invalid Bin:**\n\n**Bin -** `{input}`\n**Status -** `Invalid Bin`\n\n**Checked By -** @{me}\n**User-ID - {event.sender_id}**")
+
+print ("Successfully Started")
+bin.run_until_disconnected()
